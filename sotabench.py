010e05884031490f07abb107ef66af9c0061e094
Piotr Czapla <github@piotrczapla.com>
2019-10-09 17:23:11 +0000

    Remove chdir it shouldn't be needed.

--- sotabench.py
+++ sotabench.py
@@ -9,29 +9,30 @@ import tensorflow as tf
 
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(f"{dir_path}/tf")  # add tf. to dir path to import dynamic_eval
-os.chdir(f"{dir_path}/tf")
+sys.path.append(f"{dir_path}/tf")  # add tf. to path to import dynamic_eval
+#os.chdir(f"{dir_path}/tf")
 from dynamiceval_tf_copy_for_sotabench import dynamic_eval, data_utils, FLAGS
+#os.chdir(f"{dir_path}")
 
 def main(unused_argv=None):
-    # print("unused_argv", unused_argv)
-    # tf.logging.set_verbosity(tf.logging.INFO)
-    # # Get corpus info
-    # corpus_info = data_utils.get_corpus_info(FLAGS.corpus_info_path)
-    # n_token = corpus_info["vocab_size"]
-    # cutoffs = corpus_info["cutoffs"][1:-1]
-    # tf.logging.info("n_token {}".format(n_token))
+    print("unused_argv", unused_argv)
+    tf.logging.set_verbosity(tf.logging.INFO)
+    # Get corpus info
+    corpus_info = data_utils.get_corpus_info(FLAGS.corpus_info_path)
+    n_token = corpus_info["vocab_size"]
+    cutoffs = corpus_info["cutoffs"][1:-1]
+    tf.logging.info("n_token {}".format(n_token))
 
     evaluator = WikiText103Evaluator(
         model_name="Transformer-XL (RMS dynamic eval)",
         paper_arxiv_id="1904.08378",
         paper_pwc_id="dynamic-evaluation-of-transformer-language",
         #expected perplexity: 16.40
-    )
-    # ).eval(dynamic_eval(n_token, cutoffs, "/gpu:0"))
+    
+    ).eval(dynamic_eval(n_token, cutoffs, "/gpu:0"))
     # test why the results are not being saved, temporary mocking the evaluation
-    import numpy as np
-    evaluator._neglogloss =  np.log(16.441230035231136) * evaluator.dataset.testset_size
+    # import numpy as np
+    # evaluator._neglogloss =  np.log(16.441230035231136) * evaluator.dataset.testset_size
     evaluator.print_stats()
     evaluator.print_results()
     evaluator.save()  # to double check that it is being called
@@ -67,6 +68,7 @@ if __name__ == "__main__":
         --eval_split=test
         --same_length=True
     """.split()
+
     FLAGS(argv, known_only=True)
     assert FLAGS.data_dir == f"{dir_path}/tf/pretrained_xl/tf_wt103/data/tfrecords"
     main()
